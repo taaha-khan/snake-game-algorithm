88b95492f7fb7c006dd0029396a53e3b74bd2238
Taaha Khan <61217792+taaha-khan@users.noreply.github.com>
2020-06-05 20:47:59 -0700

    Implemented new Algorithms
    Implemented newer algorithms for snake pathfinding as well as added functions to show and hide the predicted path.

--- SnakeGameAlgorithm/Hamilton New/SnakeGameHamilton.py
+++ SnakeGameAlgorithm/Hamilton New/SnakeGameHamilton.py
@@ -7,16 +7,21 @@ from grid import Grid
 from snake import Snake
 from HamiltonianCycle import HamiltonianCycle
 
-# Screen Sizes
-scl  = 30   # Scale
+# Scaling Sizes
+scl  = 40   # Scale
 rows = 20   # Height
 cols = 30   # Width
 
+# Screen Sizes
 width = cols * scl
 height = rows * scl
 sclh = scl / 2
-speed = 1
-run = 0
+
+# Main Variables
+speed = 1; run = 0
+showingPath = False
+path = False
+start = time.time()
 
 # Main Screen Setup
 wn = turtle.Screen()
@@ -25,6 +30,7 @@ wn.title('Snake Game Algorithm')
 wn.bgcolor('#141414')
 wn.tracer(0)
 
+# Registering Scaled Square Shape
 wn.register_shape('scaled-square', ((-sclh+2, sclh-2), (sclh-2, sclh-2), (sclh-2, -sclh+2), (-sclh+2, -sclh+2)))
 
 # Generating Arena Grid Map
@@ -34,12 +40,12 @@ grid.drawBorder('red')
 
 # Generating Hamiltonian Cycle
 hc = HamiltonianCycle(grid)
-path = hc.generateHamiltonianCycle()
+hCycle = hc.generateHamiltonianCycle()
 
-snake = Snake(grid, path)
+# Creating Main Snake
+snake = Snake(grid, hCycle)
 
-start = time.time()
-
+# Speed Functions
 def speedUp():
     global speed
     speed = 10
@@ -47,6 +53,13 @@ def slowDown():
     global speed
     speed = 1
 
+# Showing Predicted Path Function
+def showPath():
+    global showingPath
+    showingPath = not showingPath
+    show.clear()
+
+# Manual Gameplay Keybindings
 def manualGameplay():
     wn.listen()
     wn.onkey(snake.up, 'Up')
@@ -55,6 +68,7 @@ def manualGameplay():
     wn.onkey(snake.right, 'Right')
     time.sleep(0.05)
 
+# Framerate Calculator
 def frameRate():
     global run, start
     end = time.time()
@@ -63,34 +77,53 @@ def frameRate():
         run = 0
         start = time.time()
  
-
+# Main Keybindings
 wn.listen()
 wn.onkeypress(speedUp, 'space')
 wn.onkeyrelease(slowDown, 'space')
+wn.onkey(showPath, 'p')
 
+# Showing predicted path pen
+show = turtle.Turtle()
+show.pu(); show.ht()
+show.color('blue'); show.width(3)
 
-
+# Main Run Loop
 while True:
 
+    # Updating Screen
     wn.update()
 
-    for s in range(speed):
+    # Speed loop
+    for play in range(speed):
 
+        # Frame Count
         run += 1
 
-        # snake.getPathFromAstar()
-        snake.shortcutHamilton()
+        # Algorithms
+        # path = snake.getPathFromAstar()
+        path = snake.shortcutHamilton()
 
+        # Showing predicted path
+        if showingPath and path:
+            show.clear()
+            show.pu(); show.goto(path[0]); show.pd()
+            for node in path:
+                show.goto(node)
+
+        # Updating Snake
         snake.run()
 
+        # Manual Game Ending
         if snake.dead:
             snake = Snake(grid, path)
 
+        # Updating Score in Title
         wn.title(f'Snake Game Algorithm  -  Score: {snake.drawnLength}')
 
+        # Runtime Functions
         # manualGameplay()
         # frameRate()
 
-
-
+# Ending Program
 wn.mainloop()
